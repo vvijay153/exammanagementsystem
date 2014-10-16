9efa1cce542cb779a60f47adfbbdf517b7ff6b9d
gautam1989 <gautamverma@Gautams-MacBook-Pro.local>
2014-10-16 19:50:00 +0800

    pDfanswers

---
+++ src/java/com/entities/PdfAnswers.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.entities;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author gautamverma
+ */
+public class PdfAnswers {
+    
+    private User studentId;
+    private Map<Question,String> questionAndAnswer;
+   
+    
+}
